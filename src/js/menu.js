e30f1bcd7c6ca5c31d5b44b030f1583496aad98e
Wallef Ryan <146087764+WallefRyan@users.noreply.github.com>
2024-06-08 00:05:35 -0300

    Add files via upload

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,6 @@
+// Recuperar o número do pedido do localStorage ou inicializar com 1
+let numeroPedido = localStorage.getItem('numeroPedido') ? parseInt(localStorage.getItem('numeroPedido')) : 1;
+
 function enviarPedido() {
     var sabor = document.querySelector('input[name="massa"]:checked').value;
     var cobertura = document.querySelector('input[name="cobertura"]:checked').value;
@@ -6,14 +9,15 @@ function enviarPedido() {
     var observacoes = document.getElementById("observacoes").value;
 
     
-    var mensagem = "Pedido de Donuts:\n";
-    mensagem += "Massa: " + sabor + "\n";
-    mensagem += "Cobertura: " + cobertura + "\n";
-    mensagem += "Recheio: " + recheio + "\n";
-    mensagem += "Quantidade: " + quantidade + "\n";
+    var mensagem = `Pedido de Donuts Nº ${numeroPedido}:\n \n \n`;
+    mensagem += `Massa: ${sabor}\n \n`;
+    mensagem += `Cobertura: ${cobertura}\n \n`;
+    mensagem += `Recheio: ${recheio}\n \n`;
+    mensagem += `Quantidade: ${quantidade}\n \n`;
     if (observacoes.trim() !== "") {
-        mensagem += "Observações: " + observacoes + "\n";
+        mensagem += `Observações: ${observacoes}\n`;
     }
+    mensagem += "\nAgradecemos pela preferência!\n";
 
     // Exibe a mensagem no console para verificar
     console.log("Mensagem: " + mensagem);
@@ -24,9 +28,13 @@ function enviarPedido() {
     // Link para enviar a mensagem para o WhatsApp
     var link = "https://api.whatsapp.com/send?phone=556192258586&text=" + mensagemCodificada + "&app_absent=0";
 
-     // Exibe o link no console para verificar
-     console.log("Link: " + link);
+    // Exibe o link no console para verificar
+    console.log("Link: " + link);
 
     // Abrir a página do WhatsApp com a mensagem preenchida
     window.open(link);
+
+    // Incrementa o número do pedido e salva no localStorage
+    numeroPedido++;
+    localStorage.setItem('numeroPedido', numeroPedido);
 }
